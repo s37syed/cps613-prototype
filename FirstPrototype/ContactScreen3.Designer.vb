a1f7383b7e267c74984fbd459adab58b26b8555b
Shah Syed <shah.u.syed@gmail.com>
2014-11-09 18:14:33 -0500

    Revert "added placeholder images"
    This reverts commit e1a1c0ae0d2e75d82f112f22e59f27bd889c3cd1.

--- FirstPrototype/ContactScreen3.Designer.vb
+++ FirstPrototype/ContactScreen3.Designer.vb
@@ -23,70 +23,28 @@ Partial Class ContactScreen3
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ContactName = New System.Windows.Forms.Label()
-        Me.ContactPhoto = New System.Windows.Forms.PictureBox()
-        Me.ContactPhone = New System.Windows.Forms.PictureBox()
-        Me.ContactMsg = New System.Windows.Forms.PictureBox()
-        CType(Me.ContactPhoto, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.ContactPhone, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.ContactMsg, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ContactName
         '
         Me.ContactName.AutoSize = True
-        Me.ContactName.Location = New System.Drawing.Point(39, 3)
+        Me.ContactName.Location = New System.Drawing.Point(33, 9)
         Me.ContactName.Name = "ContactName"
         Me.ContactName.Size = New System.Drawing.Size(35, 13)
         Me.ContactName.TabIndex = 0
         Me.ContactName.Text = "Name"
         '
-        'ContactPhoto
-        '
-        Me.ContactPhoto.BackgroundImage = Global.FirstPrototype.My.Resources.Resources.placeholder_image
-        Me.ContactPhoto.Location = New System.Drawing.Point(41, 21)
-        Me.ContactPhoto.Name = "ContactPhoto"
-        Me.ContactPhoto.Size = New System.Drawing.Size(33, 33)
-        Me.ContactPhoto.TabIndex = 1
-        Me.ContactPhoto.TabStop = False
-        '
-        'ContactPhone
-        '
-        Me.ContactPhone.BackgroundImage = Global.FirstPrototype.My.Resources.Resources.placeholder_call
-        Me.ContactPhone.Location = New System.Drawing.Point(6, 63)
-        Me.ContactPhone.Name = "ContactPhone"
-        Me.ContactPhone.Size = New System.Drawing.Size(33, 33)
-        Me.ContactPhone.TabIndex = 1
-        Me.ContactPhone.TabStop = False
-        '
-        'ContactMsg
-        '
-        Me.ContactMsg.BackgroundImage = Global.FirstPrototype.My.Resources.Resources.placeholder_text
-        Me.ContactMsg.Location = New System.Drawing.Point(75, 63)
-        Me.ContactMsg.Name = "ContactMsg"
-        Me.ContactMsg.Size = New System.Drawing.Size(33, 33)
-        Me.ContactMsg.TabIndex = 1
-        Me.ContactMsg.TabStop = False
-        '
         'ContactScreen3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Controls.Add(Me.ContactPhone)
-        Me.Controls.Add(Me.ContactMsg)
-        Me.Controls.Add(Me.ContactPhoto)
         Me.Controls.Add(Me.ContactName)
         Me.Name = "ContactScreen3"
         Me.Size = New System.Drawing.Size(113, 113)
-        CType(Me.ContactPhoto, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.ContactPhone, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.ContactMsg, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents ContactName As System.Windows.Forms.Label
-    Friend WithEvents ContactPhoto As System.Windows.Forms.PictureBox
-    Friend WithEvents ContactPhone As System.Windows.Forms.PictureBox
-    Friend WithEvents ContactMsg As System.Windows.Forms.PictureBox
 
 End Class
