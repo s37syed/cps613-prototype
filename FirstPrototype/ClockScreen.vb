ccd563fd1053f0c854ff548814d6ed660688d91a
redballs <andrew.may21@gmail.com>
2014-11-13 19:42:39 -0500

    clock can be changed back to normal colour

--- FirstPrototype/ClockScreen.vb
+++ FirstPrototype/ClockScreen.vb
@@ -1,9 +1,21 @@
 ﻿Public Class ClockScreen
+    Private IsSelected As Boolean = False
     Public Sub ClockScreen_Click(sender As Object, e As EventArgs) Handles Clock1.Click
-        Clock1.HourHand.Color = Color.Red
-        Clock1.MinuteHand.Color = Color.Green
-        Clock1.SecondHand.Color = Color.Blue
-        Clock1.CenterPoint.Color = Color.Yellow
+        If IsSelected = False Then
+            Clock1.HourHand.Color = Color.Red
+            Clock1.MinuteHand.Color = Color.Green
+            Clock1.SecondHand.Color = Color.Blue
+            Clock1.CenterPoint.Color = Color.Yellow
+            IsSelected = True
+        ElseIf IsSelected = True Then
+            Clock1.HourHand.Color = Color.Black
+            Clock1.MinuteHand.Color = Color.Black
+            Clock1.SecondHand.Color = Color.Black
+            Clock1.CenterPoint.Color = Color.Black
+            IsSelected = False
+        End If
+
+
 
     End Sub
 End Class
